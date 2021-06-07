49feb48066d411f3d5b92b42998549fb9ece4b92
Fawzi E. Abdulfattah <iifawzie@gmail.com>
2021-06-07 16:15:17 +0200

    use success instead of status

--- src/helpers/respond.ts
+++ src/helpers/respond.ts
@@ -2,7 +2,7 @@ import Express from 'express'
 
 const respond = (statusCode: number, data: any, res: Express.Response) => {
   res.status(statusCode).json({
-    status: true,
+    success: true,
     message: 'success',
     data
   })
