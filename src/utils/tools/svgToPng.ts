c97f280130fe57706549999760957a5b388a0f39
Fawzi E. Abdulfattah <iifawzie@gmail.com>
2021-06-08 21:33:54 +0200

    leave the wrtie proccess to controllers

--- src/utils/tools/svgToPng.ts
+++ src/utils/tools/svgToPng.ts
@@ -1,17 +1,15 @@
-import { promises as pfs } from 'fs'
 import pify from 'pify'
-// it doesn't have a @types, that reason we used require here.
 import svg2img from 'svg2img'
 const psvg2img = pify(svg2img)
 
-async function svgToPng (pngSize: string, svgPath: string, outputPath: string) {
+async function svgToPng (pngSize: string, svgPath: string) {
   try {
     const svgOptions = {
       width: pngSize,
       height: pngSize
     }
     const buffer = await psvg2img(svgPath, svgOptions)
-    await pfs.writeFile(outputPath, buffer)
+    return buffer
   } catch (err) {
     throw new Error(`An error occured while converting the svg to png: ${err}`)
   }
