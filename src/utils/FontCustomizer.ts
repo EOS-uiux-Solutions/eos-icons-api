6174ceabec0676daf520965266b3f164b99d35a9
Fawzi E. Abdulfattah <iifawzie@gmail.com>
2021-06-14 00:45:26 +0200

    adding the fontCustomizer class

---
+++ src/utils/FontCustomizer.ts
@@ -0,0 +1,38 @@
+import addConfigFile from './tools/addConfigFile'
+import zipFolder from './tools/zipFolder'
+
+import { tempDirectory } from '../common/constants'
+
+class FontCustomizer {
+    icons: Array<string>
+    timestamp: number
+    theme: string
+
+    constructor (icons: Array<string>, timestamp: number, theme: string) {
+      this.icons = icons
+      this.timestamp = timestamp
+      this.theme = theme
+    }
+
+    gruntCdIcons () {
+      try {
+        let command = ''
+        for (let i = 0; i < this.icons.length; i++) {
+          command += `--extended_src=${this.theme}/${this.icons[i]}.svg `
+        }
+        return `grunt -b ./src --dist=${this.timestamp} ${command} --outlined=${this.theme === 'svg-outlined'}`
+      } catch (err) {
+        throw new Error(`Some error occurred while generating Icons set command: ${err}`)
+      }
+    }
+
+    async generateFiles () {
+      const folderPath = `${tempDirectory}dist_${this.timestamp}`
+      const configFilePath = `${folderPath}/icons_config.json`
+      const zipOutputPath = `${folderPath}.zip`
+      await addConfigFile(configFilePath, JSON.stringify({ icons: this.icons, exportAs: 'font' }))
+      await zipFolder(folderPath, zipOutputPath)
+    }
+}
+
+export default FontCustomizer
