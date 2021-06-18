b9d3b57f6a6ec3711eb70276684bb54be95be591
Fawzi E. Abdulfattah <iifawzie@gmail.com>
2021-06-18 16:58:20 +0200

    adding the download-png api

--- src/components/v1/controller.v1.ts
+++ src/components/v1/controller.v1.ts
@@ -1,10 +1,12 @@
+import { promises as pfs } from 'fs'
 import Express from 'express'
 import { Logger } from 'helpers'
 import { getSvgCodePayload } from './interfaces.v1'
-import { getThemeDir, serializer } from 'utils/tools'
+import { getThemeDir, serializer, svgToPng } from 'utils/tools'
 import { iconsTheme, iconsThemeV1 } from 'common/types'
 import { SvgFactory } from 'utils'
 import { analyticsServices } from '../analytics'
+import { tempDirectory } from 'common/constants'
 const IconsLogger = new Logger('Icons Controller')
 
 const getSVGCode = async (req: Express.Request, res: Express.Response, next: Express.NextFunction) => {
@@ -35,7 +37,7 @@ const downloadSVG = async (req: Express.Request, res: Express.Response, next: Ex
     const iconName = req.params.iconName as string
     const themeDir = getThemeDir(theme)
     const iconPath = `${themeDir}/${iconName}.svg`
-    const serializedData = serializer({ icons: [iconName] }, 'svg', false)
+    const serializedData = serializer({ icons: [iconName] }, 'svg')
     analyticsServices.createAnalyticDocument(serializedData)
     res.download(iconPath)
   } catch (err) {
@@ -44,7 +46,30 @@ const downloadSVG = async (req: Express.Request, res: Express.Response, next: Ex
   }
 }
 
+const downloadPNG = async (req: Express.Request, res: Express.Response, next: Express.NextFunction) => {
+  try {
+    const iconName = req.params.iconName as string
+    const pngSize = req.params.pngSize as string
+    const theme = req.query.theme as iconsThemeV1 | iconsTheme
+    // set paths:
+    const themeDir = getThemeDir(theme)
+    const iconPath = `${themeDir}/${iconName}.svg`
+    const outputPath = `${tempDirectory}/${iconName}_${pngSize}.png`
+    // convert svg to png:
+    const pngBuffer = await svgToPng(pngSize, iconPath)
+    await pfs.writeFile(outputPath, pngBuffer)
+    // post analytics data in db:
+    const serializedData = serializer({ icons: [iconName] }, 'png')
+    analyticsServices.createAnalyticDocument(serializedData)
+    res.download(outputPath)
+  } catch (err) {
+    IconsLogger.logError('pngDownload', err)
+    next(err)
+  }
+}
+
 export {
   getSVGCode,
-  downloadSVG
+  downloadSVG,
+  downloadPNG
 }
