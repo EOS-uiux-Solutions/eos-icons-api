b537d03dbe5aafda7dbe8f89f1f1aa1c3f30bf51
Fawzi E. Abdulfattah <iifawzie@gmail.com>
2021-06-18 16:59:47 +0200

    small improvement, adding comments

--- src/components/v1/controller.v1.ts
+++ src/components/v1/controller.v1.ts
@@ -33,9 +33,11 @@ const downloadSVG = async (req: Express.Request, res: Express.Response, next: Ex
   try {
     const theme = req.query.theme as iconsThemeV1 | iconsTheme
     const iconName = req.params.iconName as string
+    // set paths:
     const themeDir = getThemeDir(theme)
     const iconPath = `${themeDir}/${iconName}.svg`
-    const serializedData = serializer({ icons: [iconName] }, 'svg', false)
+    // post analytics data to db:
+    const serializedData = serializer({ icons: [iconName] }, 'svg')
     analyticsServices.createAnalyticDocument(serializedData)
     res.download(iconPath)
   } catch (err) {
