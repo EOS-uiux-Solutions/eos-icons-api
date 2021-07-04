f39d69cfe366d8738aaaea4b74daf7286ff50698
Fawzi E. Abdulfattah <iifawzie@gmail.com>
2021-07-04 23:11:01 +0200

    message fix

--- src/components/icons/controller.icons.ts
+++ src/components/icons/controller.icons.ts
@@ -7,7 +7,7 @@ const IconsLogger = new Logger('Icons Controller')
 const newRelease = async (req: Express.Request, res: Express.Response, next: Express.NextFunction) => {
   try {
     IconsLogger.logInfo('newRelease', { message: 'A request is send to update the icons', context: req.body })
-    res.status(200).json({ message: 'update process will start' })
+    res.status(200).json({ message: 'update icons process will start' })
     updateDBIcons(true)
   } catch (err) {
     IconsLogger.logError('newRelease', err)
