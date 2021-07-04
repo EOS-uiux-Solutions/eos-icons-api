f19ca5c7618f893feb9492b058ac199f6bbabba4
Fawzi E. Abdulfattah <iifawzie@gmail.com>
2021-07-04 23:58:46 +0200

    check if the token is present, fix env template

--- src/components/icons/controller.icons.ts
+++ src/components/icons/controller.icons.ts
@@ -1,3 +1,4 @@
+import configs from 'configs'
 import Express from 'express'
 import { Logger } from 'helpers'
 import updateDBIcons from 'utils/updateDBIcons'
@@ -6,9 +7,13 @@ const IconsLogger = new Logger('Icons Controller')
 
 const newRelease = async (req: Express.Request, res: Express.Response, next: Express.NextFunction) => {
   try {
-    IconsLogger.logInfo('newRelease', { message: 'A request is send to update the icons', context: req.body })
-    res.status(200).json({ message: 'update icons process will start' })
-    updateDBIcons(true)
+    if (req.headers['X-Gitlab-Token'] === configs.GITLAP_HOOK_TOKEN) {
+      res.status(200).json({ message: 'Thanks for notifying me' })
+      IconsLogger.logInfo('newRelease', { message: 'an icons\' update is released', context: req.body })
+      updateDBIcons(true)
+    } else {
+      res.status(403).json({ message: 'Thank you for acting as Gitlab' })
+    }
   } catch (err) {
     IconsLogger.logError('newRelease', err)
     next(err)
