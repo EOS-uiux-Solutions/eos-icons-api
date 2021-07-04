707597a94be9711ea5044cdf44b163549c9ad960
Fawzi E. Abdulfattah <iifawzie@gmail.com>
2021-07-04 02:25:19 +0200

    update the comment

--- src/utils/tools/updatingIcons/isNewIcon.ts
+++ src/utils/tools/updatingIcons/isNewIcon.ts
@@ -1,5 +1,5 @@
 const isNewIcon = (iconCreationDate: string, currentVersionDate: Date) => {
-  // icon creation date's style is: DD/MM/yyyy, we need to convert it to the standard: yyyy/DD/MM
+  // icon creation date's style is: DD/MM/yyyy, we need to convert it to the standard: yyyy-DD-MM
   // to be able to make a comparison.
   const creationDate = new Date(iconCreationDate.split('/').reverse().join('-'))
   return creationDate > currentVersionDate
