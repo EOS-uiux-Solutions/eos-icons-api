22fcaeb32b40bdd55603d22b84e4359b8f5bdff0
Fawzi E. Abdulfattah <iifawzie@gmail.com>
2021-07-07 04:47:49 +0200

    adding the getAllIcons API

--- src/components/icons/router.icons.ts
+++ src/components/icons/router.icons.ts
@@ -4,6 +4,7 @@ const router = express.Router()
 
 const prefix = '/icons'
 router.get('/newReleaseGitlab', controller.newRelease)
+router.post('/getIcons', controller.getIcons)
 
 export {
   router,
