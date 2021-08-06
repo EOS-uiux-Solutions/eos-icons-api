12334ceebab054d3d4e26e13092c4534595d6bb7
Fawzi E. Abdulfattah <iifawzie@gmail.com>
2021-08-06 11:52:31 +0200

    validate the suggestions apis

--- src/components/suggestion/router.suggestion.ts
+++ src/components/suggestion/router.suggestion.ts
@@ -1,11 +1,13 @@
 import express from 'express'
 import * as controller from './controller.suggestion'
+import { validateSchema } from 'middlewares'
+import * as validationSchemas from './validation.suggestion'
 const router = express.Router()
 
 const prefix = '/suggestion'
-router.post('/add', controller.addSuggestion)
+router.post('/add', validateSchema(validationSchemas.addSuggestion, 'body'), controller.addSuggestion)
 router.get('/getAll', controller.getAllSuggestions)
-router.post('/decide', controller.decide)
+router.post('/decide', validateSchema(validationSchemas.decide, 'body'), controller.decide)
 
 export {
   router,
